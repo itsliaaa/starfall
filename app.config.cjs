853921861a149a997f926d17a4865a8bac6e5688
Lia Wynn <88979678+itsliaaa@users.noreply.github.com>
2025-11-20 16:44:35 +0700

    Correction

--- app.config.cjs
+++ app.config.cjs
@@ -2,7 +2,10 @@ module.exports = {
    apps: [{
       name: 'sf',
       script: './index.js',
-      node_args: '--max-old-space-size=1024 --max-semi-space-size=1',
+      node_args: [
+         '--max-old-space-size=512',
+         '--max-semi-space-size=8'
+      ],
       env: {
          NODE_ENV: 'bot'
       }
